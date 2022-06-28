59a08998cab0b84cc03299cb247222493cfef6a9
Jedai Saboteur <paradox.inversion@gmail.com>
2022-06-28 11:13:31 -0700

    Added baseline prometheus code

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+const Prometheus = require('prom-client');
+const metricsInterval = Prometheus.collectDefaultMetrics();
 
 // Constants
 const PORT = 8080;
@@ -11,6 +13,11 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', Prometheus.register.contentType)
+  res.end(await Prometheus.register.metrics())
+})
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
